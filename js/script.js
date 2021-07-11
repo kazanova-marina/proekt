8d74847c65d6657c95b6cb96dc4d7e220fb7c711
kazanova-marina <kazanova.marina_0@mail.ru>
2021-07-11 23:33:49 +0300

    added very much files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('.submenu_mover').click(function(){
+        if ($(this).parent().hasClass('open')) {
+            $('.catmenu_item.open').removeClass('open').find('.submenu').animate({
+                height: 0
+            }, 1000);
+        } else {
+            $('.catmenu_item.open').removeClass('open').find('.submenu').animate({
+                height: 0
+            }, 1000);
+            $(this).parent().addClass('open').find('.submenu').animate({
+                height: ($(this).parent().find('.submenu a').length * 24)
+            }, 1000);
+        }
+    });
+	
+	$('#privet').click(function(){alert('Привет!')});
+});
